e014759dfa742567cca6ac0551dfa9774afee6d2
Eric Minassian <eric@ericminassian.com>
2024-05-19 17:23:26 -0700

    refactor: binary application

--- src/error.rs
+++ src/error.rs
@@ -10,3 +10,23 @@ pub enum Error {
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Error::UnexpectedToken { expected, found } => {
+                write!(
+                    f,
+                    "Unexpected token: expected {:?}, found {:?}",
+                    expected, found
+                )
+            }
+            Error::UnexpectedEndOfFile => {
+                write!(f, "Unexpected end of file")
+            }
+            Error::SyntaxError(msg) => {
+                write!(f, "Syntax error: {}", msg)
+            }
+        }
+    }
+}
